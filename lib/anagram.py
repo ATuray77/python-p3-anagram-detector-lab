3fcb7f958bdd57a742688fdd2a5d2cae40ab85a1
Aruna Turay <tura_arun@yahoo.com>
2023-07-25 12:09:37 -0400

    all passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+# your code goes here!class Anagram:
+
+class Anagram:
+
+  def __init__(self, word):
+    self.word = word
+
+  def match(self, words):
+    return [word for word in words if sorted(self.word) == sorted(word)]
